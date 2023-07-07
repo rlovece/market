5794fb03315ca31c1618769e88a6cbf5a42a7ec7
rlovece <ruth.lovece@gmail.com>
2023-07-07 16:05:40 -0300

    Se expone api y peticiones

--- src/main/java/com/ruth/market/web/controller/ProductController.java
+++ src/main/java/com/ruth/market/web/controller/ProductController.java
@@ -4,8 +4,9 @@ import com.ruth.market.domain.Product;
 import com.ruth.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,23 +18,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    public List<Product> getAll(){
-        return productService.getAll();
+    @GetMapping("/all") //se utiliza GetMapping para obtener info
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity(productService.getAll(), HttpStatus.OK);
     }
 
-    public Optional<Product> getProduct (int productId){
+    @GetMapping("/productId/{id}") ///agregando el @PathVariable logro que colocando el id en URL se ejecute el método
+    public Optional<Product> getProduct (@PathVariable("id") int productId){
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory (int categoryId){
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory (@PathVariable("categoryId") int categoryId){
         return productService.getByCategory(categoryId);
     }
 
-    public Product save (Product product){
+    @PostMapping("/save")
+    public Product save (@RequestBody Product product){
         return productService.save(product);
     }
 
-    public boolean delete (int productId){
+    @DeleteMapping("/delete/{id}")
+    public boolean delete (@PathVariable("id") int productId){
         return productService.delete(productId);
     }
 }
